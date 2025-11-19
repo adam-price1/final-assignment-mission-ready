2a983c2ad76daa487c1e52d295b4cdb936515670
dawnjords <taikijordan@gmail.com>
2025-11-19 20:52:07 +1300

    front end login page and sign up pages done

--- mission5_frontend/src/components/LoginPage.jsx
+++ mission5_frontend/src/components/LoginPage.jsx
@@ -1,7 +1,51 @@
-import React from "react";
+import startUpBanner from "/image/banners/startupbanner.png";
+import signInImage from "/image/signUpImage.png";
+import styles from "./LoginPage.module.css";
 
 function LoginPage() {
-  return <div>LoginPage</div>;
+  return (
+    <div>
+      <section className={styles.loginBanner}>
+        <img src={startUpBanner} alt="Login Banner" className={styles.banner} />
+        <h1 className={styles.bannerText}>Login</h1>
+      </section>
+
+      <main className={styles.signIn}>
+        <section className={styles.signInLeft}>
+          <form className={styles.signInForm}>
+            <label htmlFor="email">
+              E-mail
+              <input
+                id="email"
+                name="email"
+                type="email"
+                placeholder="Please enter your email address here"
+                required
+              />
+            </label>
+            <label htmlFor="password">
+              Password
+              <input
+                id="password"
+                name="password"
+                type="password"
+                placeholder="Please enter your password here"
+                required
+              />
+            </label>
+            <div className={styles.signInButton}>
+              <button type="submit" className={styles.login}>
+                Login
+              </button>
+            </div>
+          </form>
+        </section>
+        <section className={styles.signInRight}>
+          <img src={signInImage} alt="Person signing in on a tablet" />
+        </section>
+      </main>
+    </div>
+  );
 }
 
 export default LoginPage;
